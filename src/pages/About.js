6b5aef9b954d3193747897c0e90b2a0fd6d2a52b
Feriel <hadjarabferiel@gmail.com>
2024-05-12 10:47:30 +0200

    Récupération des données de l'API et leur traitement

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sequi nulla quas minima earum sunt? Voluptatibus dignissimos tenetur eius assumenda omnis ab molestiae sed nesciunt, adipisci itaque exercitationem minus veniam voluptate commodi suscipit odit expedita ipsa fugiat earum cupiditate id. Non repellendus dicta ullam libero error repudiandae sunt nisi ex tempora voluptatem eligendi porro maiores, quia nobis, possimus vitae odit deleniti dolorum? Recusandae mollitia sunt cumque fugiat, modi et nesciunt sapiente exercitationem ea explicabo molestias quae atque eligendi enim corporis quo dolorem harum ab odio eius accusantium eveniet alias rem impedit. Laudantium autem porro distinctio culpa, et repellendus eligendi nobis.</p>
+            <br />
+        </div>
+    );
+};
+
+export default About;
